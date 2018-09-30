734fe7b34464d9d946c1c02cc11563f6f4289300
jksyeap <j.ks.yeap@gmail.com>
2018-09-30 10:41:57 +0000

    added base.njk, foodmenu.njk, and added pictures

--- build.js
+++ build.js
@@ -3,11 +3,15 @@ var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var openHours = require("./hours.json");
 var nav = require("./nav.json");
+var food = require("./foodmenu.json");
+var pictures = require("./pictures.json");
 
 MetalSmith(__dirname)
   .metadata({
     hours: openHours,
     links: nav,
+    menu: food,
+    pics: pictures,
   })
   .source('./src')
   .destination('./output')
